3455906bb901840b4112a87f4084b1f7860354de
kuwako <kuwako@leverages.jp>
2017-04-10 19:43:52 +0900

    りんごで動いた

--- exec.py
+++ exec.py
@@ -4,6 +4,8 @@ import gensim
 
 model = gensim.models.KeyedVectors.load_word2vec_format('model.vec', binary=False)
 
-model.most_similar(positive=['tennis'])
-# japaneseはなぜか判定なし
+print(model.most_similar(positive=['リンゴ']))
+
+# japaneseもtennisもなぜか判定なし
+# model.most_similar(positive=['tennis'])
 # model.most_similar(positive=['japanese'])
